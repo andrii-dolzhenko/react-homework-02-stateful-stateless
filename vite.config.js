0ac9a777dc3f5c648eed0dd3d189e29833483bc0
andrii-dolzhenko <andrii.dolzhenko@gmail.com>
2026-08-25 10:49:40 +0300

    ci: deploy TaskFlow to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github-pages' ? '/react-homework-02-stateful-stateless/' : '/',
   plugins: [react()],
-})
+}))
